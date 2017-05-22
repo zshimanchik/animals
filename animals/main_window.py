373d3f787557b2da668817a43e47ed75e0c23ec4
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2017-05-22 16:36:20 +0300

    increased max recursion limit, for pickling

--- animals/main_window.py
+++ animals/main_window.py
@@ -15,6 +15,18 @@ import world
 from population_graph_window import PopulationGraphWindow
 from world_constants import WorldConstants
 
+import resource
+import sys
+
+print(resource.getrlimit(resource.RLIMIT_STACK))
+print(sys.getrecursionlimit())
+
+max_rec = 0x100000
+
+# May segfault without this line. 0x100 is a guess at the size of each stack frame.
+resource.setrlimit(resource.RLIMIT_STACK, [0x100 * max_rec, resource.RLIM_INFINITY])
+sys.setrecursionlimit(max_rec)
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     _PERFORMANCE_CALC_INTERVAL = 20
