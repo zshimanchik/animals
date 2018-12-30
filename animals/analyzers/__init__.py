bf508975a9061c2d6074a17cdd69e3db0f7f9bf2
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2018-12-30 02:34:01 +0100

    mammoth analyzer implemented

---
+++ animals/analyzers/__init__.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+class MammothAnalyzer:
+    def __init__(self, world, analysis_interval=2000):
+        self.world = world
+        self.analysis_interval = analysis_interval
+        self.killing_history = deque([0] * self.analysis_interval, maxlen=self.analysis_interval)
+        self.killing_history_sum = 0
+        self._prev_mammoth_amount = len(self.world.mammoths)
+
+    def update(self):
+        killed_mammoths_amount = max(0, self._prev_mammoth_amount - len(self.world.mammoths))
+        self._prev_mammoth_amount = len(self.world.mammoths)
+        self.killing_history_sum += -self.killing_history[0] + killed_mammoths_amount
+        self.killing_history.append(killed_mammoths_amount)
+
+    @property
+    def amount_of_killings(self):
+        return self.killing_history_sum / self.analysis_interval
