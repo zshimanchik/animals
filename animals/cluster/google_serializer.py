a98e32787850b17290927f4290cada2b40f1a7c1
Zakhar Shymanchyk <zahar.shimanchik@yandex.ru>
2020-11-22 17:46:27 +0100

    Making temp file with random name for google_serializer

--- animals/cluster/google_serializer.py
+++ animals/cluster/google_serializer.py
@@ -1,30 +1,33 @@
 import os
 
 from google.cloud import storage
+import random
 
 import engine.serializer as file_serializer
 
-TEMP_FILENAME = 'tmp.wrld'
+TEMP_FILENAME_PATTERN = 'tmp{}.wrld'
 
 
 def save(world, path):
     """
     Saves world and upload to google bucket
     """
-    file_serializer.save(world, TEMP_FILENAME)
+    temp_filename = TEMP_FILENAME_PATTERN.format(random.randint(0, 200))
+    file_serializer.save(world, temp_filename)
     client = storage.Client()
     blob = storage.Blob.from_string(path, client=client)
-    blob.upload_from_filename(TEMP_FILENAME)
-    os.remove(TEMP_FILENAME)
+    blob.upload_from_filename(temp_filename)
+    os.remove(temp_filename)
 
 
 def load(path):
     """
     Loads world from google bucket
     """
+    temp_filename = TEMP_FILENAME_PATTERN.format(random.randint(0, 200))
     client = storage.Client()
     blob = storage.Blob.from_string(path, client=client)
-    blob.download_to_filename(TEMP_FILENAME)
-    result = file_serializer.load(TEMP_FILENAME)
-    os.remove(TEMP_FILENAME)
+    blob.download_to_filename(temp_filename)
+    result = file_serializer.load(temp_filename)
+    os.remove(temp_filename)
     return result
