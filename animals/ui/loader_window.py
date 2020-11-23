177bfe8361ea4b744100c935d32cb981480dfbf4
Zakhar Shymanchyk <zahar.shimanchik@yandex.ru>
2020-11-23 11:09:28 +0100

    Better loader window with two lists and filter

--- animals/ui/loader_window.py
+++ animals/ui/loader_window.py
@@ -1,7 +1,7 @@
+import os
+
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot as Slot
-from PyQt5.QtCore import Qt
-import os
 
 from ui.loader_window_ui import Ui_LoaderWindow
 
@@ -20,32 +20,65 @@ class LoaderWindow(Ui_LoaderWindow, QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setupUi(self)
-        self.init_list(self.parent().snapshot_directory_combobox.currentText())
-        self.treeWidget.itemDoubleClicked.connect(self.item_clicked)
+        self._db = {}
+        self.init_db(self.parent().snapshot_directory_combobox.currentText())
+        self.update_ui_with_new_db()
+        self.listWidget.currentItemChanged.connect(self.tw_current_item_changed)
+        self.listWidget2.currentItemChanged.connect(self.list_widget2_current_item_changed)
+        self.lineEdit.textChanged.connect(self.line_edit_text_changed)
 
-    def init_list(self, snapshot_dir):
-        self.treeWidget.clear()
+        # self.print_latest('201122')
+
+    def init_db(self, snapshot_dir):
+        self._db.clear()
         for dirpath, dirnames, filenames in os.walk(snapshot_dir):
             dirnames.sort()
-            item = QtWidgets.QTreeWidgetItem(self.treeWidget)
+            item = []
             dirpath = dirpath[len(snapshot_dir)+1:]
-            item.setText(0, dirpath)
+            self._db[dirpath] = item
             for filename in sorted((f for f in filenames if f.endswith('.wrld')), key=sort_key):
-                subitem = QtWidgets.QTreeWidgetItem(item)
-                subitem.setText(0, filename)
+                item.append(filename)
 
-            # print(dirpath, filename)
+    def update_ui_with_new_db(self):
+        self.listWidget.clear()
+        self.listWidget2.clear()
 
-    @Slot(QtWidgets.QTreeWidgetItem, int)
-    def item_clicked(self, item: QtWidgets.QTreeWidgetItem, column):
-        if not item.text(0).endswith('.wrld'):
+        filter = self.lineEdit.text()
+        filtered_db = (wn for wn in self._db if filter in wn)
+        for dirpath in filtered_db:
+            item = QtWidgets.QListWidgetItem(self.listWidget)
+            item.setText(dirpath)
+
+    @Slot(QtWidgets.QListWidgetItem, QtWidgets.QListWidgetItem)
+    def tw_current_item_changed(self, cur: QtWidgets.QListWidgetItem, prev: QtWidgets.QListWidgetItem):
+        self.listWidget2.clear()
+
+        if cur is None:
             return
 
-        if isinstance(item.parent(), QtWidgets.QTreeWidgetItem):
-            path = os.path.join(item.parent().text(0), item.text(0))
-        else:
-            path = './' + item.text(0)
-        filename = os.path.join(self.parent().snapshot_directory_combobox.currentText(), path)
+        dirpath = cur.text()
+        for world_time in self._db[dirpath]:
+            item = QtWidgets.QListWidgetItem()
+            item.setText(world_time)
+            self.listWidget2.addItem(item)
+
+    @Slot(QtWidgets.QListWidgetItem, QtWidgets.QListWidgetItem)
+    def list_widget2_current_item_changed(self, cur: QtWidgets.QListWidgetItem, prev: QtWidgets.QListWidgetItem):
+        if cur is None:
+            return
+
+        world_name = self.listWidget.currentItem().text()
+        world_time = cur.text()
+        filename = os.path.join(self.parent().snapshot_directory_combobox.currentText(), world_name, world_time)
+        print('Loading filename: ', filename)
         self.parent().load_world(filename)
 
+    @Slot(str)
+    def line_edit_text_changed(self, text):
+        self.update_ui_with_new_db()
+
+    def print_latest(self, filter_text=''):
+        for dirname, worlds in self._db.items():
+            if filter_text in dirname:
+                print(dirname, '-', worlds[-1][:-len('.wrld')])
 
